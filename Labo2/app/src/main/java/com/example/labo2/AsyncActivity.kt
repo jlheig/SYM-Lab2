3043d385a139d5eaf75662279e3422c788445fb1
Klab93 <kylian.bourcoud@gmail.com>
2021-11-02 15:34:28 +0100

    3.1 done

--- Labo2/app/src/main/java/com/example/labo2/AsyncActivity.kt
+++ Labo2/app/src/main/java/com/example/labo2/AsyncActivity.kt
@@ -2,14 +2,32 @@ package com.example.labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import java.net.CacheResponse
 
 class AsyncActivity : AppCompatActivity() {
+
+    lateinit var sendButton : Button
+    lateinit var requestText : EditText
+    lateinit var receptionText : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async)
-    }
 
-    fun sendAsyncMessage() {
+        requestText = findViewById(R.id.async_sendContent)
+        receptionText = findViewById(R.id.async_receptionContent)
+        sendButton = findViewById(R.id.async_sendButton)
 
+        sendButton.setOnClickListener {
+            val scm = SymComManager( object : CommunicationEventListener {
+                override fun handleServerResponse(response : String) {
+                    receptionText.setText(response)
+                }
+            })
+
+            scm.sendRequest( requestText.getText().toString(), "http://mobile.iict.ch/api/txt" )
+        }
     }
 }
